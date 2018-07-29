a6cd5ef6c91ef09cceb2adea3517f807940fc80b
Samuil Gospodinov <samuil4@gmail.com>
2018-07-29 21:21:40 +0300

    adding missing export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { Model } from './Model';
+export { Model, cachedPropery } from './Model';
 export { Collection } from './Collection';
 export { IModelOptions } from './interfaces/IModelOptions';
 export { IRawData } from './interfaces/IRawData';
