13d1760a112165f4e8cf573dd4384aa11d394b37
Samuil Gospodinov <samuil4@gmail.com>
2018-07-29 18:37:30 +0300

    preparing for package publishing

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { Model } from './Model';
+export { Collection } from './Collection';
+export { IModelOptions } from './interfaces/IModelOptions';
+export { IRawData } from './interfaces/IRawData';
+export { ICollectionOptions } from './interfaces/ICollectionOptions';
